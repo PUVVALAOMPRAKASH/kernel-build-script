38881a16d9bb9a9d4ae57ba71ca7fe52487ea545
Puvvala Omprakash <166001286+PUVVALAOMPRAKASH@users.noreply.github.com>
2025-11-10 02:06:22 +0530

    Refactor kernel cloning script for clarity and structure

--- clone.sh
+++ clone.sh
@@ -1,9 +1,17 @@
-# Kernel Clonning Script
+#!/bin/bash
 
+# Simple Kernel Cloning Script - OmKernel
+
+cd /home/omrak325/KernelBuild || exit
+
+# Clone Kernel Source
 git clone https://github.com/karthik558/MsM-4.14-RyZeN- RyZeN
-git clone --depth=1 https://github.com/karthik558/AnyKernel3 Anykernel
+
+# Clone AnyKernel3 (for packaging)
+git clone --depth=1 https://github.com/karthik558/AnyKernel3 AnyKernel
+
+# Clone Clang Toolchain
 git clone --depth=1 https://github.com/kdrag0n/proton-clang CLANG-13
 
-# Start Build
-
+# Start the build
 bash run.sh
